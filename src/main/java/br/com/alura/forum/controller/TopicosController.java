44786c35854eec087b397123ce6ac31e90767ca1
Erick Fernandes <erick.farias@dcx.ufpb.br>
2022-08-27 09:47:08 -0300

    API rest with Spring Boot

--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -1,20 +1,22 @@
 package br.com.alura.forum.controller;
 
+import br.com.alura.forum.controller.dto.DetalhesDoTopicoDto;
+import br.com.alura.forum.controller.dto.TopicoDto;
+import br.com.alura.forum.controller.form.AtualizacaoTopicoForm;
 import br.com.alura.forum.controller.form.TopicoForm;
-import br.com.alura.forum.modelo.Curso;
 import br.com.alura.forum.modelo.Topico;
 import br.com.alura.forum.repository.CursoRepository;
 import br.com.alura.forum.repository.TopicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import javax.validation.Valid;
 import java.net.URI;
-import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/topicos")//todos os métodos começarão com /topicos
@@ -54,6 +56,7 @@ public class TopicosController {
 
 
     @PostMapping//Vai adicionar topicos
+    @Transactional
     public ResponseEntity<TopicoDto> cadastrar(@RequestBody @Valid TopicoForm form, UriComponentsBuilder uriBuilder) {
 
         Topico topico = form.converter(cursoRepository);
@@ -63,9 +66,68 @@ public class TopicosController {
         return ResponseEntity.created(uri).body(new TopicoDto(topico));
 
     }
+
+//    @GetMapping("/{id}")//detalhando topicos
+//    public TopicoDto detalhar(@PathVariable Long id) {
+//
+//        Topico topico = topicoRepository.getOne(id);
+//
+//        return new TopicoDto(topico);
+//
+//    }
+
+//    @GetMapping("/{id}")//detalhando topicos
+//    public DetalhesDoTopicoDto detalhar(@PathVariable Long id) {
+//
+//        Topico topico = topicoRepository.getReferenceById(id);
+////        topicoRepository.getOne(id) --> o getOne está deprecated.
+//
+//        return new DetalhesDoTopicoDto(topico);
+//
+//    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<DetalhesDoTopicoDto> detalhar(@PathVariable Long id) {
+        Optional<Topico> topico = topicoRepository.findById(id);
+
+        if (topico.isPresent()) {
+            return ResponseEntity.ok(new DetalhesDoTopicoDto(topico.get()));
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+    @PutMapping("/{id}")//Vai atualizar novos dados de um tópico
+    @Transactional//avisa ao Spring que é pra commitar uma transação no final do método
+    public ResponseEntity<TopicoDto> atualizar(@PathVariable Long id, @RequestBody @Valid AtualizacaoTopicoForm form) {
+
+        Optional<Topico> optional = topicoRepository.findById(id);
+
+        if (optional.isPresent()) {
+            Topico topico = form.atualizar(id, topicoRepository);
+            //corpo que será devolvido como resposta pelo servidor
+            return ResponseEntity.ok(new TopicoDto(topico));
+
+
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+
+    @DeleteMapping("/{id}")//Vai deletar um tópico
+    @Transactional
+    public ResponseEntity<?> remover(@PathVariable Long id) {
+        Optional<Topico> topico = topicoRepository.findById(id);
+
+        if (topico.isPresent()) {
+            topicoRepository.deleteById(id);
+            return ResponseEntity.ok().build();
+        }
+        return ResponseEntity.notFound().build();
+    }
 //    @RequestBody -> corpo da requisição;  é para pegar do corpo da requisição, e
 //    não das URLs, como parâmetro de URL.
 //    @Valid --> Rodar as validações do bean validation
-
-
+// obs! é de suma importancia colocar a transação nos métodos adicionar, deletar e atualizar;
+//  Vai que troco de provedor ou banco de dados e para de funcionar. É bom garantir que esses
+//  três métodos rodarão dentro de uma transação.
 }
--- src/main/java/br/com/alura/forum/repository/TopicoRepository.java
+++ src/main/java/br/com/alura/forum/repository/TopicoRepository.java
@@ -1,11 +1,7 @@
 package br.com.alura.forum.repository;
 
-import br.com.alura.forum.controller.TopicoDto;
 import br.com.alura.forum.modelo.Topico;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
-import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
